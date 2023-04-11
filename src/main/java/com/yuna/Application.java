4fd416d40fba2a78fe5eb21dfb47d5c2b37daef7
yuna706 <yuna706@danusys.com>
2023-04-11 17:05:30 +0900

    feat: 의존성 추가

---
+++ src/main/java/com/yuna/Application.java
@@ -0,0 +1,18 @@
+package com.yuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application.java
+ * spring boot application
+ *
+ * @author 이유나
+ * @since 2023.03.31
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
